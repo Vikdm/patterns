774f739ac5faa2539c3f8e42a55664f9c78854e1
vikdm <viktorov@my.com>
2022-08-07 15:01:57 +0300

    Faker.Delivery v.3

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -24,6 +24,7 @@ class CardDeliveryTest {
     public String generateDate(int days) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
+
     @BeforeEach
     void openWebBank() {
         open("http://localhost:7777/");
@@ -32,9 +33,8 @@ class CardDeliveryTest {
 
     @Test
     void testDelivery() {
-        Configuration.timeout = 15;
-        String planDateMeet = generateDate(5);
-        String planDateMeet1 = generateDate(10);
+        String planDateMeet = generateDate(6);
+        String planDateMeet1 = generateDate(12);
 
         $("[data-test-id=city] input").val(faker.address().city());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
@@ -47,8 +47,29 @@ class CardDeliveryTest {
         $x("//input[@type='tel']").val(planDateMeet1);
         $(".button.button_view_extra.button_size_m").click();
         $(".button.button_view_extra.button_size_s").click();
-        $(".notification__content")
-                .shouldHave(Condition.text("Встреча успешно забронирована на " + planDateMeet1), Duration.ofSeconds(15))
+        $(".notification__title")
+                .shouldHave(Condition.text("Успешно!"), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+    }
+
+    @Test
+    void testDelivery1() {
+        String planDateMeet = generateDate(10);
+        String planDateMeet1 = generateDate(5);
+
+        $("[data-test-id=city] input").val(faker.address().city());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x("//input[@type='tel']").val(planDateMeet);
+        $x("//input[@name='name']").val(faker.name().fullName());
+        $x("//input[@name='phone']").val(faker.phoneNumber().phoneNumber());
+        $("[data-test-id=agreement]").click();
+        $(".button.button_view_extra.button_size_m.button_theme_alfa-on-white").click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x("//input[@type='tel']").val(planDateMeet1);
+        $(".button.button_view_extra.button_size_m").click();
+        $(".button.button_view_extra.button_size_s").click();
+        $(".notification__title")
+                .shouldHave(Condition.text("Успешно!"), Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
     }
 }
